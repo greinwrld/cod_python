23683bf070f423c1dac59ad9e78f2233c1727686
greinwrld <vitorgrein04@outlook.com>
2024-05-23 22:01:52 -0300

    Add files via upload

---
+++ arquivo_txt.py
@@ -0,0 +1,16 @@
+class ArquivoTexto(object):
+
+  def __init__(self, arquivo: str):
+    self.arquivo = arquivo
+    self.conteudo = self.extrair_conteudo(arquivo)
+
+  def extrair_conteudo(self, arquivo):
+    with open(arquivo, 'r') as file:
+        conteudo = file.read()
+    return conteudo
+
+  def extrair_linha(self, numero_linha: int):
+    lista_com_vazios = conteudo.split(sep='\n')
+    lista_sem_vazios = [item for item in lista_com_vazios if item]
+    extrair_linha = lista_sem_vazios[numero_linha]
+    return extrair_linha
